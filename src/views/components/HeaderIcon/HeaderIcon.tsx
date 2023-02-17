81e3b8f5736dcb6d27cf41879d9c3313f5680fa8
Mario Ramos <mario.ramos@sophossolutions.com>
2023-02-17 14:52:24 -0500

    borrar store en cierre sesion

--- src/views/components/HeaderIcon/HeaderIcon.tsx
+++ src/views/components/HeaderIcon/HeaderIcon.tsx
@@ -2,14 +2,21 @@ import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 import {TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {useDispatch} from 'react-redux';
 import {colors} from '../../../globalStyles';
+import {clear} from '../../../infrastructure/store/slices/favorites/favoritesSlice';
 
 export const HeaderIcon = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+  const salir = () => {
+    dispatch(clear());
+    navigation.getParent()?.navigate('LoginScreen');
+  };
 
   return (
-    <TouchableOpacity
-      onPress={() => navigation.getParent()?.navigate('LoginScreen')}>
+    <TouchableOpacity onPress={salir}>
       <Icon
         name="flower"
         style={{
